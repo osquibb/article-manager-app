ecc71a4315970e7a997642534788b571ceba6e0f
Oliver Squibb <osquibb@gmail.com>
2019-05-06 17:29:23 -0400

    clear search functionality

--- src/components/FolderSectionComponent.js
+++ src/components/FolderSectionComponent.js
@@ -25,21 +25,13 @@ class FolderSection extends React.Component {
   }
 
   getWikiArticles(searchTerm) {
-    fetch(`https://en.wikipedia.org/w/api.php?&origin=*&action=opensearch&search=${searchTerm}&limit=4`)
-    .then(resp => resp.json())
-    .then(data => {
-      const newArticles = {};
-      const newFolder = {
-        id: 'folder-1',
-        title: '',
-        articleIds: []
-      };
-      for(let i=0; i < data[1].length; i++) {
-        const next = this.state.nextArticleId;
-        newArticles[`article-${next}`] = {id: `article-${next}`, content: data[1][i], link: data[3][i]};
-        newFolder.articleIds.push(`article-${next}`);
-        this.setState(prevState => ({nextArticleId: prevState.nextArticleId + 1}));
-      }
+    const newArticles = {};
+    const newFolder = {
+      id: 'folder-1',
+      title: '',
+      articleIds: []
+    };
+    if (searchTerm === '') {
       const newState = {
         ...this.state,
         articles: {...this.state.articles, ...newArticles},
@@ -48,10 +40,31 @@ class FolderSection extends React.Component {
           'folder-1': newFolder
         }
       };
-      return newState;
-    })
-    .then(newState => this.setState(newState))
-    .catch(err => console.log(err));
+      this.setState(newState);
+    } 
+    else {
+      fetch(`https://en.wikipedia.org/w/api.php?&origin=*&action=opensearch&search=${searchTerm}&limit=4`)
+      .then(resp => resp.json())
+      .then(data => {
+        for(let i=0; i < data[1].length; i++) {
+          const next = this.state.nextArticleId;
+          newArticles[`article-${next}`] = {id: `article-${next}`, content: data[1][i], link: data[3][i]};
+          newFolder.articleIds.push(`article-${next}`);
+          this.setState(prevState => ({nextArticleId: prevState.nextArticleId + 1}));
+        }
+        const newState = {
+          ...this.state,
+          articles: {...this.state.articles, ...newArticles},
+          folders: {
+            ...this.state.folders,
+            'folder-1': newFolder
+          }
+        };
+        return newState;
+      })
+      .then(newState => this.setState(newState))
+      .catch(err => console.log(err));
+    }
   }
 
   deleteArticle(articleId) {
