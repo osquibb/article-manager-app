a489def7b7ab862c5bf4fc8e6ac3e14108a42c66
Oliver Squibb <osquibb@gmail.com>
2019-05-08 16:20:10 -0400

    Updates

---
+++ precache-manifest.0f567bde685cdff3476de801c1a39219.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f8a7442c3c7d3661b3ad728653d2825",
+    "url": "/article-manager-app/index.html"
+  },
+  {
+    "revision": "6da10f689322991f8971",
+    "url": "/article-manager-app/static/css/2.34dc5f7e.chunk.css"
+  },
+  {
+    "revision": "da7d9b6c0361d2db5259",
+    "url": "/article-manager-app/static/css/main.82984c8c.chunk.css"
+  },
+  {
+    "revision": "6da10f689322991f8971",
+    "url": "/article-manager-app/static/js/2.61f0c8d2.chunk.js"
+  },
+  {
+    "revision": "da7d9b6c0361d2db5259",
+    "url": "/article-manager-app/static/js/main.4f87465a.chunk.js"
+  },
+  {
+    "revision": "567b275a20bbfead80ec",
+    "url": "/article-manager-app/static/js/runtime~main.4ce29075.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/article-manager-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/article-manager-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/article-manager-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/article-manager-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/article-manager-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
